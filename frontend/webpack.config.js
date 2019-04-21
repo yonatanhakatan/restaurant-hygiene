d1a2118fd9acedf967337c2fce90bd4a5b9587c5
Jonny Levy <iamjonnylevy@gmail.com>
2019-04-21 23:25:55 +0100

    Add pwa manifest

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const WebpackPwaManifest = require("webpack-pwa-manifest")
+const path = require("path")
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -40,6 +42,56 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "public/index.html",
       filename: "index.html"
+    }),
+    new WebpackPwaManifest({
+      short_name: "Restaurant Hygiene",
+      name: "Restaurant Hygiene",
+      start_url: "/",
+      display: "standalone",
+      theme_color: "#000000",
+      background_color: "#000000",
+      icons: [
+        {
+          src: path.resolve("./public/images/icon-72x72.png"),
+          sizes: "72x72",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-96x96.png"),
+          sizes: "96x96",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-128x128.png"),
+          sizes: "128x128",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-144x144.png"),
+          sizes: "144x144",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-152x152.png"),
+          sizes: "152x152",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-192x192.png"),
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-384x384.png"),
+          sizes: "384x384",
+          type: "image/png"
+        },
+        {
+          src: path.resolve("./public/images/icon-512x512.png"),
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
     })
   ]
 }
