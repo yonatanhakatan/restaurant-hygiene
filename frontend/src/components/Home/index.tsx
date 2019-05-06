50178b16603d2ca42cea9517007048522dec6009
Jonny Levy <iamjonnylevy@gmail.com>
2019-05-06 18:07:54 +0100

    Create places Input

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,18 +1,43 @@
 import React, { PureComponent } from "react"
 import LoadGoogleMapsApi from "load-google-maps-api"
 
+import PlacesInput from "../PlacesInput"
+
 interface Props {}
 
-export default class Home extends PureComponent<Props> {
+interface State {
+  gmapsInitialised: boolean
+  latLng: google.maps.LatLngLiteral | null
+}
+
+export default class Home extends PureComponent<Props, State> {
   constructor(props: Props) {
     super(props)
+    this.state = {
+      gmapsInitialised: false,
+      latLng: null
+    }
     LoadGoogleMapsApi({
       libraries: ["places"],
       key: "AIzaSyDPS4DMawKvu72-uQnN2QqAyG7Bfps9KXI"
+    }).then(() => {
+      this.setState({
+        gmapsInitialised: true
+      })
+    })
+  }
+
+  handleInputLatLng = (latLng: google.maps.LatLngLiteral) => {
+    this.setState({
+      latLng
     })
   }
 
   render() {
-    return <div>Hello</div>
+    return (
+      this.state.gmapsInitialised && (
+        <PlacesInput onLatLng={this.handleInputLatLng} />
+      )
+    )
   }
 }
---
+++ frontend/src/components/PlacesInput/index.tsx
@@ -0,0 +1,71 @@
+import React, { PureComponent } from "react"
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete"
+
+interface Props {
+  onLatLng: (latLng: google.maps.LatLngLiteral) => void
+}
+
+interface State {
+  address: string
+}
+
+export default class PlacesInput extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      address: ""
+    }
+  }
+
+  handleChange = (address: string) => {
+    this.setState({ address })
+  }
+
+  handleSelect = (address: string) => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => this.props.onLatLng(latLng))
+  }
+
+  render() {
+    return (
+      <PlacesAutocomplete
+        searchOptions={{
+          componentRestrictions: { country: "gb" }
+        }}
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: "Search Places ..."
+              })}
+            />
+            <div>
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const style = suggestion.active
+                  ? { backgroundColor: "#ff0000", cursor: "pointer" }
+                  : { backgroundColor: "#ff00ff", cursor: "pointer" }
+                const { key, ...props } = getSuggestionItemProps(suggestion, {
+                  style
+                })
+                return (
+                  <div key={key} {...props}>
+                    <span>{suggestion.description}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    )
+  }
+}
